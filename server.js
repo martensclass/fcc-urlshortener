2d215ca1027d8835f1aa42ebb3eae8df349e7a81
John Martens <jmartens@scdsb.on.ca>
2016-06-19 12:38:49 +0000

    finished the url shortener project

--- server.js
+++ server.js
@@ -1,33 +1,85 @@
-'use strict';
-
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
+var mongoose = require('mongoose');
+var url = 'mongodb://localhost:27017/data/db'; 
+mongoose.connect(url);
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
+var urlSchema = mongoose.Schema({
+    shortcut: String,
+    url: String
+});
+var Shortcut = mongoose.model("Shortcut", urlSchema);
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
+function getRandpath(){
+  var path="";
+  for(var x=1; x<=4; x++){
+    while(true){
+      var num=Math.floor(Math.random() * (122-49)) + 49;
+      var bad = ((num>=58 && num <=64) || (num>=91 && num <=96) || num==111 || num==79);
+      if(!bad)break;
+    }
+    path+=String.fromCharCode(num);
+  }
+  return path;
+}
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/index.html');
+});
 
-routes(app, passport);
+function checkurl(u){
+  var template = "/new/http://www.";
+  var rest=u.slice(16);
+  var f11=u.slice(0,16);
+  return (f11===template && checkend(rest));
+}
 
-var port = process.env.PORT || 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+function checkend(rest){
+  var len=rest.length;
+  return ((len>=6) && (rest[len-3]=="." || rest[len-4]=="." || rest[len-5]==".")) ;
+}
+
+app.get('*', function (req, res) {
+  var newurl = req.params["0"];
+   //res.json({url: newurl, length: newurl.length});
+
+  var something= getRandpath();
+  if(newurl.slice(0,4) != '/new'){
+    //trying to go to a shortcut
+    if(newurl.length !=5)
+    res.json({url: "Are you looking for a shortcut?  If so, they are only 4 characters long.  If you want to make a NEW shortcut you need to start with /new/http://"});
+    else{
+      //look for shortcut
+      Shortcut.findOne({shortcut: "https://fcc-urlshortener-martensclass.c9users.io" + newurl},function(err,sc){
+        if(err) console.log(err);
+        else if(!sc){
+           res.json({url: "Sorry, the shortcut " + newurl + " does not exist."});
+        }
+        else{
+          res.redirect(sc.url);
+        }
+      });
+    }
+  }
+  else{ //try to create a new shortcut
+    if(checkurl(newurl)){
+        newurl = newurl.slice(5);
+        var entry = new Shortcut({shortcut: "https://fcc-urlshortener-martensclass.c9users.io/" + something, url: newurl });
+        entry.save(function(err,ent){
+        if(err) console.log(err);
+            else{
+              res.json({shortcut: "https://fcc-urlshortener-martensclass.c9users.io/" + something, url: newurl});
+           }
+        });
+    }
+    else
+        res.json({url: "invalid url - must start with: /new/http://www. and end with a properly formatted domain such as .com, .info, .io, .co.uk, .ca, etc.."});
+        
+  }
+    
+});
+
+app.listen(8080, function () {
+    console.log('Listening on port 8080...');
 });
